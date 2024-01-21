46fe39305b011d8e592a1eaebb3e21ff32310399
jaeyeong13 <141209201+jaeyeong13@users.noreply.github.com>
2024-01-22 02:21:15 +0900

    CustomUser Model 설정

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import User
 
 # Register your models here.
+@admin.register(User)     # 관리자 페이지에 수동으로 User 모델 등록하기
+class CustomUserAdmin(UserAdmin):
+  fieldsets = [
+    (None, {'fields': ('username', 'password')}),
+    ('개인정보', {'fields': ('first_name', 'last_name', 'email')}),
+    ('추가필드', {'fields': ('profile_image', 'short_description')}),
+    (
+      '권한',
+      {
+        'fields': (
+          'is_active',
+          'is_staff',
+          'is_superuser',
+        )
+      },
+    ),
+    ('중요한 일정', {'fields': ('last_login', 'date_joined')}),
+  ]
--- users/models.py
+++ users/models.py
@@ -7,4 +7,7 @@ class User(AbstractUser):
   AbstractUser는 Django가 CustomUser 모델을 만들기 위해 제공하는 기본 유저 형태를 가진 모델 클래스이다.
   이 클래스는 Django의 기본 User모델이 가진 필드를 똑같이 가지고 있으며, AbstractUser를 상속받으면 자동적으로 username, password, first_name, last_name, email, is_staff(관리자 여부), is_active(활성화 여부), date_joined, last_login 필드들이 모델에 추가된다.
   '''
-  pass
+  profile_image = models.ImageField(
+    '프로필 이미지', upload_to='users/profile', blank=True
+  )
+  short_description = models.TextField('소개글', blank=True)   # 여기까지 하고, 모델을 변경했으므로 변경사항에 대한 migration을 생성하고 적용한다.
