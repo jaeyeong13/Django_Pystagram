593e54a2e277061af1493cb8aa0cf5cf0cd6dcea
jaeyeong13 <141209201+jaeyeong13@users.noreply.github.com>
2024-01-22 04:20:36 +0900

    로그인 여부에 따라 페이지 접속 제한 되게 만들기

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def feeds(request):
+  if not request.user.is_authenticated:
+    # 사용자가 로그인하지 않은 경우, 로그인 페이지로 이동시킴
+    return redirect('/users/login/')
   return render(request, 'posts/feeds.html')
--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def login_view(request):
+  # 이미 로그인되어 있다면 피드 페이지로 이동시킴
+  if request.user.is_authenticated:
+    return redirect('/posts/feeds/')
   return render(request, 'users/login.html')
