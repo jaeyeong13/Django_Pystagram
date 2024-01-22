75efbd0191a4de5736129fe98d5556473c507053
jaeyeong13 <141209201+jaeyeong13@users.noreply.github.com>
2024-01-23 04:14:30 +0900

    [feat] thumbnail 이미지 표시

--- posts/admin.py
+++ posts/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from posts.models import Post, PostImage, Comment
+from django.contrib.admin.widgets import AdminFileWidget
+from django.db import models
+from django.utils.safestring import mark_safe
+import admin_thumbnails
 
 # Register your models here.
 class CommentInline(admin.TabularInline):   # ForeignKey로 연결된 다른 객체들을 보려면 admin의 Inline 기능을 이용한다. 여기서의 CommentInline은 Comment를 볼 수 있는 Inline 클래스이다.
   model = Comment
   extra = 1
 
+@admin_thumbnails.thumbnail('photo')
 class PostImageInline(admin.TabularInline):
   model = PostImage
   extra = 1
