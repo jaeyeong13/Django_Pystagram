bf37b48ac0eb825caecff07d88edf90c6ff9926b
jaeyeong13 <141209201+jaeyeong13@users.noreply.github.com>
2024-01-22 20:52:16 +0900

    logout 구현 및 login 개선

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 # app별로 urls.py 파일의 내용을 분리한다.
 from django.urls import path
-from users.views import login_view
+from users.views import login_view, logout_view
 
 urlpatterns = [
   path('login/', login_view),
+  path('logout/', logout_view),
 ]
