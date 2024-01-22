f37a8caf10a1db9f0c1bfe0b2ccbb550a9e39339
jaeyeong13 <141209201+jaeyeong13@users.noreply.github.com>
2024-01-23 03:32:23 +0900

    [feat] 관리자 페이지에서 ForeignKey를 이용하여 연결된 객체들을 한 번에 확인할 수 있도록 수정

--- posts/admin.py
+++ posts/admin.py
@@ -2,11 +2,23 @@ from django.contrib import admin
 from posts.models import Post, PostImage, Comment
 
 # Register your models here.
+class CommentInline(admin.TabularInline):   # ForeignKey로 연결된 다른 객체들을 보려면 admin의 Inline 기능을 이용한다. 여기서의 CommentInline은 Comment를 볼 수 있는 Inline 클래스이다.
+  model = Comment
+  extra = 1
+
+class PostImageInline(admin.TabularInline):
+  model = PostImage
+  extra = 1
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
   list_display = [
     'id',
     'content',
+  ]
+  inlines = [
+    CommentInline,
   ]
 
 
