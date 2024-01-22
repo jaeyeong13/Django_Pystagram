0e1d0be5474c2f64e3d2c7c92d079b4bf61c23fd
jaeyeong13 <141209201+jaeyeong13@users.noreply.github.com>
2024-01-23 04:31:52 +0900

    [feat] Splide를 이용해서 image slider 구현

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
+from posts.models import Post
 
 # Create your views here.
 def feeds(request):
   if not request.user.is_authenticated:
     # 사용자가 로그인하지 않은 경우, 로그인 페이지로 이동시킴
     return redirect('/users/login/')
-  return render(request, 'posts/feeds.html')
+  posts = Post.objects.all()
+  context = {'posts': posts}
+  return render(request, 'posts/feeds.html', context)
