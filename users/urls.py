c6731086d3ac1e46e3632c2a58d799c4ba1d9b05
jaeyeong13 <141209201+jaeyeong13@users.noreply.github.com>
2024-01-23 01:33:57 +0900

    회원가입 기능 구현

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 # app별로 urls.py 파일의 내용을 분리한다.
 from django.urls import path
-from users.views import login_view, logout_view
+from users.views import login_view, logout_view, signup
 
 urlpatterns = [
   path('login/', login_view),
   path('logout/', logout_view),
+  path('signup/', signup),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from users.forms import LoginForm
+from users.forms import LoginForm, SignupForm
+from users.models import User
 
 # Create your views here.
 def login_view(request):
@@ -30,3 +31,23 @@ def login_view(request):
 def logout_view(request):
   logout(request)
   return redirect('/users/login/')
+
+def signup(request):
+  # POST 요청 시, form이 유효하다면 최종적으로 redirect 처리된다.
+  if request.method == 'POST':
+    form = SignupForm(data=request.POST, files=request.FILES)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('/posts/feeds/')
+    # POST 요청에서 form이 유효하지 않다면, 아래의 context = ... 부분으로 이동한다.
+
+  # GET 요청 시, 빈 form을 생성한다.
+  else:
+    form=SignupForm()
+
+  # context로 전달되는 form은 두 가지 경우가 존재한다.
+  # 1. POST 요청에서 생성된 form이 유효하지 않은 경우 - 에러를 포함한 form이 사용자에게 보여진다.
+  # 2. GET 요청으로 빈 form이 생성된 경우 - 빈 form이 사용자에게 보여진다.
+  context = {'form': form}
+  return render(request, 'users/signup.html', context)
